447c76b1e66c0535faec81efe1f85e0b3706e8b9
MichealI23 <Micheal.imoe@gmail.com>
2024-02-26 23:50:12 +0000

    Dogs

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "dog.h"
+
+
+/**
+ * _strlen - ...
+ * @s: ...
+ *
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	d->name = fred;
+	d->age = "2";
+        d->owner = Brad
